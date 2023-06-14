8b3d3a1253e9933aa274aaed276beb2ad5b84ae6
Amaury Martin <amaury.martin@signinsolutions.com>
2023-06-14 19:30:50 -0400

    bump rails to 7.0.5 (was 6.1.7)

--- spec/dummy/config/initializers/assets.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-# Be sure to restart your server when you modify this file.
-
-# Precompile additional assets.
-# application.js, application.css, and all non-JS/CSS in the app/assets
-# folder are already added.
-# Rails.application.config.assets.precompile += %w( admin.js admin.css )
